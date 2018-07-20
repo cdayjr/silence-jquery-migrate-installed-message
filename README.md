b4f5b2129aa0aa10a0236bddd43ec3c036c03ce3
Chad Wade Day, Jr <cdayjr@users.noreply.github.com>
2018-07-20 16:31:33 -0700

    Fist release

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# silence-jquery-migrate-installed-message
-A WordPress plugin to silence the "JQMIGRATE: Migrate is installed" console message.
+# Silence jQuery Migrate Installed Message
+
+A simple WordPress plugin to silence the "JQMIGRATE: Migrate is installed" console message.
+
+### Installing
+
+Just drop this folder into your WordPress plugin folder and activate it. You should no longer see the "JQMIGRATE: Migrate is installed" message.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details
+
+## Acknowledgments
+
+* [jquery - Annoying JQMIGRATE: Migrate is in console after update to WP 4.5 - WordPress Development Stack Exchange](https://wordpress.stackexchange.com/a/224719) - This answer has the base idea for the plugin, I just decided to package it into a plugin.
---
+++ silence-jquery-migrate-installed-message.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Silence jQuery Migrate Installed Message
+ *
+ * @author      Chad Wade Day, Jr.
+ * @copyright   2018 Chad Wade Day, Jr.
+ * @license     MIT
+ *
+ * @wordpress-plugin
+ * Plugin Name: Silence jQuery Migrate Installed Message
+ * Plugin URI:  https://github.com/cdayjr/silence-jquery-migrate-installed-message
+ * Description: Silence the "JQMIGRATE: Migrate is installed" console message.
+ * Version:     1.0.0
+ * Author:      Chad Wade Day, Jr.
+ * Author URI:  https://www.chadwadedayjr.info
+ * Text Domain: silence-jquery-migrate-installed-message
+ * License:     MIT
+ * License URI: https://raw.githubusercontent.com/cdayjr/silence-jquery-migrate-installed-message/master/LICENSE
+ */
+
+defined('ABSPATH') or die('No direct script access allowed.');
+
+// Remove jQuery Migrate is Installed message
+function silence_jquery_migrate_installed_message()
+{
+    wp_add_inline_script(
+        'jquery-migrate',
+        <<<'EOD'
+window.console.log2 = window.console.log;
+window.console.log = function(entry) {
+    if (entry.indexOf("JQMIGRATE: Migrate is installed") === -1) {
+        window.console.log2(entry);
+    }
+}
+EOD
+        ,
+        'before'
+    );
+    wp_add_inline_script(
+        'jquery-migrate',
+        <<<'EOD'
+window.console.log = window.console.log2;
+delete window.console.log2;
+EOD
+        ,
+        'after'
+    );
+}
+add_action('wp_enqueue_scripts', 'silence_jquery_migrate_installed_message');
+add_action('admin_enqueue_scripts', 'silence_jquery_migrate_installed_message');
+
